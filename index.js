5e7d98d684b25ebe7478a7e546aa2ee37faaf0da
evgisbergen <86548320+evgisbergen@users.noreply.github.com>
2021-08-14 20:22:40 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+console.log("Hello Winc Academy")
+VM921: 1 Hello Winc Academy
+undefined
+let name = 'evelien';
+undefined
+console.log(evelien);
+VM1042: 1 Uncaught ReferenceError: evelien is not defined
+at < anonymous >: 1: 13
+    (anonymous)	@VM1042: 1
+console.log(name);
+VM1102: 1 evelien
+undefined
+4 + 4
+8
+"4" + "4+"
+"44+"
+//zijn 2 strings
+undefined
+8 + 7
+15
+console.log(15)
+VM1236: 1 15
+undefined
+8 - 7
+1
+console.log(1)
+VM1300: 1 1
+undefined
+8 * 7
+56
+console.log(56)
+VM1373: 1 56
+undefined
+56 / 8
+7
+console.log(7)
+VM1477: 1 7
+undefined
+let age = 32;
+undefined
+console.log(32)
+VM1558: 1 32
+undefined
+typeof age
+"number"
+let age = thirtytwo;
+VM1619: 1 Uncaught ReferenceError: thirtytwo is not defined
+at < anonymous >: 1: 9
+let age = "thirtytwo";
+undefined
+
+typeof age
+typeof age
+"string"
