eea977653b5926798058a18803af96f9da2f0cd0
evgisbergen <86548320+evgisbergen@users.noreply.github.com>
2021-08-15 18:02:52 +0200

    Add files via upload

---
+++ kroeg.js
@@ -0,0 +1,24 @@
+const age = "16";
+if (age > 18) {
+    console.log("You can come in");
+} else if (age == 18) {
+    console.log("You can come in");
+} else {
+    console.log("you are too young, please go");
+}
+
+const isFemale = false;
+if (isFemale) {
+    console.log("Welcome inn Lady");
+} else {
+    console.log("Sorry dude, it is lady night");
+}
+
+const driverStatus = "1";
+if (driverStatus < 1) {
+    console.log("You can be BoB");
+} else if (driverStatus == 1) {
+    console.log("you can be Bob");
+} else {
+    console.log("You can't drive and need to find a BoB");
+}
