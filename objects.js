37917c4152c843788724d07ab90e940e5c877a87
evgisbergen <86548320+evgisbergen@users.noreply.github.com>
2021-08-22 21:34:08 +0200

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,35 @@
+let person = {
+    name: 'Evelien',
+    age: 32,
+    evaluations: [7, 10, 9]
+};
+
+console.log(person);
+
+let name = 'evelien';
+console.log(name)
+let age = '32';
+console.log(age)
+
+console.log(person.name)
+console.log['age'];
+
+let evaluations = [7, 10, 9];
+console.log(evaluations)
+
+let selectedColors = ['groen', 'blauw', 'rood']
+console.log(selectedColors)
+console.log(selectedColors.length)
+console.log(selectedColors[0])
+console.log(selectedColors[selectedColors.length - 1])
+
+let count = selectedColors.push('geel');
+console.log(count);
+console.log(selectedColors);
+selectedColors.push(1);
+console.log(selectedColors);
+selectedColors.push({ greeting: "hi ik ben een object" });
+console.log(selectedColors);
+console.log(selectedColors[selectedColors.length - 1])
+
+console.log()
