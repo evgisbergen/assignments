e483634c2b376ce9a8cae84e701cfdf742c0b820
evgisbergen <86548320+evgisbergen@users.noreply.github.com>
2021-08-23 21:54:49 +0200

    Add files via upload

---
+++ arguments.js
@@ -0,0 +1,16 @@
+const paint_Walls = function (item, item2) {
+    console.log(item2 + " has been painted " + item);
+}
+
+paint_Walls();
+const paintcolour = "green"
+const paintcolour2 = "yellow"
+const location = "The right wall"
+const location2 = "The left wall"
+
+paint_Walls(paintcolour, location2);
+paint_Walls(paintcolour2, location);
+
+
+
+
