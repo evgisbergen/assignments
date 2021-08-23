9252522d53630682602f58ecb938fd1240d929fa
evgisbergen <86548320+evgisbergen@users.noreply.github.com>
2021-08-23 11:05:55 +0200

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,21 @@
+console.log("How to make sushi");
+const makeSushi = function () {
+    console.log("cook rice");
+    console.log("cut salmon & cucumber");
+    console.log("stick rice around ingredients");
+    console.log("cover it with Yaki Nori")
+};
+makeSushi();
+makeSushi();
+makeSushi();
+
+console.log("How to make Pizza");
+const makePizza = function () {
+    console.log("make dough");
+    console.log("make sauce");
+    console.log("turn oven on with 220 degrees");
+    console.log("decorate dough with sauze and ingredients");
+    console.log("place in the oven for 15 min")
+};
+makePizza();
+makePizza();
